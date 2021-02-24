8ee89f225be2004d3e8ecd560f6822229d01e1c6
Jon Cram <webignition@gmail.com>
2021-02-24 17:16:52 +0000

    Create empty symfony project (#2)

---
+++ ecs.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(Option::SETS, [
+        SetList::CLEAN_CODE,
+        SetList::PSR_12,
+    ]);
+
+    $parameters->set(Option::PATHS, [
+        __DIR__ . '/bin',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+};

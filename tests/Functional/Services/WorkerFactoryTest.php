b4dff5d8e555b95dbb6d6130a2a6d9da4b34e08d
Jon Cram <webignition@gmail.com>
2021-03-09 14:44:02 +0000

    Create WorkerFactory

---
+++ tests/Functional/Services/WorkerFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Services;
+
+use App\Model\ProviderInterface;
+use App\Services\WorkerFactory;
+use App\Tests\Functional\AbstractBaseFunctionalTest;
+use webignition\ObjectReflector\ObjectReflector;
+
+class WorkerFactoryTest extends AbstractBaseFunctionalTest
+{
+    private WorkerFactory $workerFactory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $workerFactory = self::$container->get(WorkerFactory::class);
+        if ($workerFactory instanceof WorkerFactory) {
+            $this->workerFactory = $workerFactory;
+        }
+    }
+
+    public function testCreate(): void
+    {
+        $label = md5('label content');
+        $provider = ProviderInterface::NAME_DIGITALOCEAN;
+
+        $worker = $this->workerFactory->create($label, $provider);
+
+        self::assertIsInt(ObjectReflector::getProperty($worker, 'id'));
+    }
+}

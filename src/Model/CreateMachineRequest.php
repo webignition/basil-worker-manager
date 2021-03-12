e844458aa27f12aa15032f9e673527382369f492
Jon Cram <webignition@gmail.com>
2021-03-12 09:33:51 +0000

    Create CreateMachineRequest

---
+++ src/Model/CreateMachineRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Model;
+
+class CreateMachineRequest
+{
+    public function __construct(
+        private int $workerId,
+        private int $retryCount = 0,
+    ) {
+    }
+
+    public function getWorkerId(): int
+    {
+        return $this->workerId;
+    }
+
+    public function getRetryCount(): int
+    {
+        return $this->retryCount;
+    }
+}

1df9d17f3bab5207edd01fa17e19b11aed4562e6
Jon Cram <webignition@gmail.com>
2021-03-15 10:25:02 +0000

    Use http fixtures in DropFactory functional test

--- tests/Functional/Services/MachineProvider/DigitalOcean/DropletFactoryTest.php
+++ tests/Functional/Services/MachineProvider/DigitalOcean/DropletFactoryTest.php
@@ -4,12 +4,19 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Services\MachineProvider\DigitalOcean;
 
+use App\Model\ProviderInterface;
 use App\Services\MachineProvider\DigitalOcean\DropletFactory;
+use App\Services\WorkerFactory;
 use App\Tests\AbstractBaseFunctionalTest;
+use DigitalOceanV2\Entity\Droplet as DropletEntity;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Psr7\Response;
 
 class DropletFactoryTest extends AbstractBaseFunctionalTest
 {
     private DropletFactory $dropletFactory;
+    private WorkerFactory $workerFactory;
+    private MockHandler $mockHandler;
 
     protected function setUp(): void
     {
@@ -19,10 +26,43 @@ class DropletFactoryTest extends AbstractBaseFunctionalTest
         if ($dropletFactory instanceof DropletFactory) {
             $this->dropletFactory = $dropletFactory;
         }
+
+        $workerFactory = self::$container->get(WorkerFactory::class);
+        if ($workerFactory instanceof WorkerFactory) {
+            $this->workerFactory = $workerFactory;
+        }
+
+        $mockHandler = self::$container->get(MockHandler::class);
+        if ($mockHandler instanceof MockHandler) {
+            $this->mockHandler = $mockHandler;
+        }
     }
 
-    public function testVerifyServiceExistsInContainer(): void
+    public function testCreateSuccess(): void
     {
-        self::assertInstanceOf(DropletFactory::class, $this->dropletFactory);
+        $dropletData = [
+            'droplet' => [
+                'id' => 456,
+            ],
+        ];
+
+        $expectedDropletEntity = new DropletEntity($dropletData);
+
+        $this->mockHandler->append(
+            new Response(
+                200,
+                [
+                    'content-type' => 'application/json',
+                ],
+                (string) json_encode([
+                    'droplet' => $expectedDropletEntity->toArray(),
+                ])
+            ),
+        );
+
+        $worker = $this->workerFactory->create(md5('label content'), ProviderInterface::NAME_DIGITALOCEAN);
+        $createDropletEntity = $this->dropletFactory->create($worker);
+
+        self::assertSame($expectedDropletEntity->id, $createDropletEntity->id);
     }
 }

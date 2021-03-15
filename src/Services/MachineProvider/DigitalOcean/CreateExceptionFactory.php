26488f52134945a07d26035d4bcd5572b010a413
Jon Cram <webignition@gmail.com>
2021-03-15 16:55:32 +0000

    Use ApiLimitExceededException

--- src/Services/MachineProvider/DigitalOcean/CreateExceptionFactory.php
+++ src/Services/MachineProvider/DigitalOcean/CreateExceptionFactory.php
@@ -4,11 +4,20 @@ namespace App\Services\MachineProvider\DigitalOcean;
 
 use App\Entity\Worker;
 use App\Exception\MachineProvider\CreateException;
+use App\Exception\MachineProvider\DigitalOcean\ApiLimitExceededException;
 use App\Exception\MachineProvider\DigitalOcean\DropletLimitExceededException;
+use DigitalOceanV2\Client;
+use DigitalOceanV2\Exception\ApiLimitExceededException as VendorApiLimitExceededException;
 use DigitalOceanV2\Exception\ExceptionInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class CreateExceptionFactory
 {
+    public function __construct(
+        private Client $digitalOceanClient,
+    ) {
+    }
+
     public function create(Worker $worker, ExceptionInterface $exception): CreateException
     {
         if (DropletLimitExceededException::is($exception)) {
@@ -16,6 +25,19 @@ class CreateExceptionFactory
                 $worker,
                 new DropletLimitExceededException($exception)
             );
+        }
+
+        if ($exception instanceof VendorApiLimitExceededException) {
+            $lastResponse = $this->digitalOceanClient->getLastResponse();
+            if ($lastResponse instanceof ResponseInterface) {
+                return new CreateException(
+                    $worker,
+                    new ApiLimitExceededException(
+                        (int) $lastResponse->getHeaderLine('RateLimit-Reset'),
+                        $exception
+                    )
+                );
+            }
         }
 
         return new CreateException($worker, $exception);

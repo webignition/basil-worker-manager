14220e5298eaaf415fd91dbd968fc3b8b51ad062
Jon Cram <webignition@gmail.com>
2021-03-16 14:02:40 +0000

    Implement DigitalOceanMachineProvider::hydrate()

--- tests/Functional/Services/MachineProvider/DigitalOcean/DigitalOceanMachineProviderTest.php
+++ tests/Functional/Services/MachineProvider/DigitalOcean/DigitalOceanMachineProviderTest.php
@@ -71,4 +71,37 @@ class DigitalOceanMachineProviderTest extends AbstractBaseFunctionalTest
         self::assertSame($remoteId, $this->worker->getRemoteId());
         self::assertSame($ipAddresses, ObjectReflector::getProperty($this->worker, 'ip_addresses'));
     }
+
+    public function testHydrateSuccess(): void
+    {
+        $remoteId = 123;
+        $ipAddresses = ['127.0.0.1', '10.0.0.1', ];
+
+        self::assertNull($this->worker->getRemoteId());
+        self::assertSame([], ObjectReflector::getProperty($this->worker, 'ip_addresses'));
+
+        $dropletData = [
+            'id' => $remoteId,
+            'networks' => (object) [
+                'v4' => [
+                    (object) [
+                        'ip_address' => $ipAddresses[0],
+                        'type' => 'public',
+                    ],
+                    (object) [
+                        'ip_address' => $ipAddresses[1],
+                        'type' => 'public',
+                    ],
+                ],
+            ],
+        ];
+
+        $expectedDropletEntity = new DropletEntity($dropletData);
+        $this->mockHandler->append(HttpResponseFactory::fromDropletEntity($expectedDropletEntity));
+
+        $this->worker = $this->machineProvider->hydrate($this->worker);
+
+        self::assertSame($remoteId, $this->worker->getRemoteId());
+        self::assertSame($ipAddresses, ObjectReflector::getProperty($this->worker, 'ip_addresses'));
+    }
 }

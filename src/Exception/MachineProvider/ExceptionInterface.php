7c15bcbc278b5c527bbce3afa7718d280218ae96
Jon Cram <webignition@gmail.com>
2021-03-19 10:09:25 +0000

    Create ExceptionInterface::getAction() (#158)

--- src/Exception/MachineProvider/ExceptionInterface.php
+++ src/Exception/MachineProvider/ExceptionInterface.php
@@ -4,5 +4,14 @@ namespace App\Exception\MachineProvider;
 
 interface ExceptionInterface
 {
+    public const ACTION_CREATE = 'create';
+    public const ACTION_GET = 'get';
+    public const ACTION_DELETE = 'delete';
+
     public function getRemoteException(): \Throwable;
+
+    /**
+     * @return self::ACTION_*
+     */
+    public function getAction(): string;
 }

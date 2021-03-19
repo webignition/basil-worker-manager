96ab596dbf4db4403bf4fdc61d1b6feb38346cf2
Jon Cram <webignition@gmail.com>
2021-03-19 12:37:41 +0000

    Add back necessary constructor

--- src/Exception/MachineProvider/DigitalOcean/HttpException.php
+++ src/Exception/MachineProvider/DigitalOcean/HttpException.php
@@ -4,9 +4,23 @@ namespace App\Exception\MachineProvider\DigitalOcean;
 
 use App\Exception\MachineProvider\Exception;
 use App\Exception\MachineProvider\HttpExceptionInterface;
+use App\Model\MachineProviderActionInterface;
+use DigitalOceanV2\Exception\RuntimeException;
 
 class HttpException extends Exception implements HttpExceptionInterface
 {
+    /**
+     * @param MachineProviderActionInterface::ACTION_* $action
+     */
+    public function __construct(
+        string $resourceId,
+        string $action,
+        int $code,
+        RuntimeException $remoteException
+    ) {
+        parent::__construct($resourceId, $action, $code, $remoteException);
+    }
+
     public function getStatusCode(): int
     {
         return $this->getRemoteException()->getCode();

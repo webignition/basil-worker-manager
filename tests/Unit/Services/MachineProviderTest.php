77c9ea7e0c1181517b5af4e91ef5cd6e62ab1295
Jon Cram <webignition@gmail.com>
2021-03-19 18:24:22 +0000

    Tidy up

--- tests/Unit/Services/MachineProviderTest.php
+++ tests/Unit/Services/MachineProviderTest.php
@@ -7,6 +7,7 @@ namespace App\Tests\Unit\Services;
 use App\Entity\Worker;
 use App\Exception\UnsupportedProviderException;
 use App\Model\ProviderInterface;
+use App\Services\ExceptionFactory\MachineProvider\ExceptionFactory;
 use App\Services\MachineProvider\MachineProvider;
 use PHPUnit\Framework\TestCase;
 
@@ -14,7 +15,7 @@ class MachineProviderTest extends TestCase
 {
     public function testCreateThrowsUnsupportedProviderException(): void
     {
-        $machineProvider = new MachineProvider([]);
+        $machineProvider = new MachineProvider([], \Mockery::mock(ExceptionFactory::class));
 
         $worker = Worker::create(md5('id content'), ProviderInterface::NAME_DIGITALOCEAN);
 

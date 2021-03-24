0cc37f70047bafb1d13ec2e409956c41347ed545
Jon Cram <webignition@gmail.com>
2021-03-24 12:14:34 +0000

    Remove delegation in MachineRequestMessageHandler

--- src/MessageHandler/MachineRequestMessageHandler.php
+++ src/MessageHandler/MachineRequestMessageHandler.php
@@ -5,32 +5,18 @@ declare(strict_types=1);
 namespace App\MessageHandler;
 
 use App\Message\MachineRequestMessage;
-use App\Services\MachineHandler\RequestHandlerInterface;
+use App\Services\MachineHandler\RequestHandler;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class MachineRequestMessageHandler implements MessageHandlerInterface
 {
-    /**
-     * @var RequestHandlerInterface[]
-     */
-    private array $handlers;
-
-    /**
-     * @param RequestHandlerInterface[] $handlers
-     */
-    public function __construct(array $handlers)
-    {
-        $this->handlers = array_filter($handlers, function ($value) {
-            return $value instanceof RequestHandlerInterface;
-        });
+    public function __construct(
+        private RequestHandler $requestHandler
+    ) {
     }
 
     public function __invoke(MachineRequestMessage $message): void
     {
-        foreach ($this->handlers as $handler) {
-            if ($handler->handles($message->getType())) {
-                $handler->handle($message->getRequest());
-            }
-        }
+        $this->requestHandler->handle($message->getRequest());
     }
 }

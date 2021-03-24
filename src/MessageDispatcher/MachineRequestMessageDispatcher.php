5f3678a5716efdf8e03eaeebb825baf7c60db5c2
Jon Cram <webignition@gmail.com>
2021-03-24 17:10:39 +0000

    Add initial dispatch delay to MachineRequestMessageDispatcher

--- src/MessageDispatcher/MachineRequestMessageDispatcher.php
+++ src/MessageDispatcher/MachineRequestMessageDispatcher.php
@@ -14,22 +14,42 @@ class MachineRequestMessageDispatcher
     public function __construct(
         private MessageBusInterface $messageBus,
         private int $dispatchDelayInSeconds = 0,
+        private ?int $initialDispatchDelayInSeconds = null,
         private bool $enabled = true
     ) {
     }
 
     public function dispatch(MachineRequestInterface $message): void
     {
-        if ($this->enabled) {
-            $stamps = [];
-
-            if ($this->dispatchDelayInSeconds > 0) {
-                $stamps = [
-                    new DelayStamp($this->dispatchDelayInSeconds * 1000)
-                ];
-            }
-
-            $this->messageBus->dispatch(new Envelope($message, $stamps));
+        if (false === $this->enabled) {
+            return;
         }
+
+        $stamps = [];
+        $dispatchDelay = $this->getDispatchDelay($message);
+
+        if ($dispatchDelay > 0) {
+            $stamps = [
+                new DelayStamp($dispatchDelay * 1000)
+            ];
+        }
+
+        $this->messageBus->dispatch(new Envelope($message, $stamps));
+    }
+
+    private function getDispatchDelay(MachineRequestInterface $request): int
+    {
+        if (0 === $request->getRetryCount()) {
+            return $this->getInitialDispatchDelay();
+        }
+
+        return $this->dispatchDelayInSeconds;
+    }
+
+    private function getInitialDispatchDelay(): int
+    {
+        return is_int($this->initialDispatchDelayInSeconds)
+            ? $this->initialDispatchDelayInSeconds
+            : $this->dispatchDelayInSeconds;
     }
 }

4a1ea697ee39f634e60047c4033e0f62c263477c
Jon Cram <webignition@gmail.com>
2021-03-31 11:31:27 +0100

    Add UnprocessableRequestExceptionInterface::getReason()

--- src/Exception/MachineProvider/UnprocessableRequestExceptionInterface.php
+++ src/Exception/MachineProvider/UnprocessableRequestExceptionInterface.php
@@ -4,4 +4,8 @@ namespace App\Exception\MachineProvider;
 
 interface UnprocessableRequestExceptionInterface
 {
+    public const CODE_REMOTE_PROVIDER_RESOURCE_LIMIT_REACHED = 100;
+    public const REASON_REMOTE_PROVIDER_RESOURCE_LIMIT_REACHED = 'remote provider resource limit reached';
+
+    public function getReason(): string;
 }
---
+++ tests/Unit/Exception/MachineProvider/DigitalOcean/DropletLimitExceededExceptionTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Exception\MachineProvider\DigitalOcean;
+
+use App\Exception\MachineProvider\DigitalOcean\DropletLimitExceededException;
+use App\Exception\MachineProvider\UnprocessableRequestExceptionInterface;
+use App\Model\RemoteRequestActionInterface;
+use DigitalOceanV2\Exception\ValidationFailedException;
+use PHPUnit\Framework\TestCase;
+
+class DropletLimitExceededExceptionTest extends TestCase
+{
+    private DropletLimitExceededException $exception;
+
+    protected function setUp(): void
+    {
+        $providerException = new ValidationFailedException(
+            'creating this/these droplet(s) will exceed your droplet limit',
+            422
+        );
+
+        $this->exception = new DropletLimitExceededException(
+            'machine id',
+            RemoteRequestActionInterface::ACTION_CREATE,
+            $providerException
+        );
+    }
+
+    public function testGetCode(): void
+    {
+        self::assertSame(
+            UnprocessableRequestExceptionInterface::CODE_REMOTE_PROVIDER_RESOURCE_LIMIT_REACHED,
+            $this->exception->getCode()
+        );
+    }
+
+    public function testGetReason(): void
+    {
+        self::assertSame(
+            UnprocessableRequestExceptionInterface::REASON_REMOTE_PROVIDER_RESOURCE_LIMIT_REACHED,
+            $this->exception->getReason()
+        );
+    }
+}

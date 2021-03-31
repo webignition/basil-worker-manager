69c0b880116ec969fa5062a8a07a1bbe7b5c6200
Jon Cram <webignition@gmail.com>
2021-03-31 15:58:24 +0100

    Use Use webignition/symfony-messenger-json-message-serializer-bundle

--- src/Message/CheckMachineIsActive.php
+++ src/Message/CheckMachineIsActive.php
@@ -4,6 +4,19 @@ declare(strict_types=1);
 
 namespace App\Message;
 
+use webignition\JsonMessageSerializerBundle\Message\JsonSerializableMessageInterface;
+
 class CheckMachineIsActive extends AbstractMachineRequest
 {
+    public const TYPE = 'check-machine-is-active';
+
+    public function getType(): string
+    {
+        return self::TYPE;
+    }
+
+    public static function createFromArray(array $data): JsonSerializableMessageInterface
+    {
+        return new CheckMachineIsActive($data['machine_id']);
+    }
 }
--- src/Message/DeleteMachine.php
+++ src/Message/DeleteMachine.php
@@ -5,13 +5,39 @@ declare(strict_types=1);
 namespace App\Message;
 
 use App\Model\RemoteRequestActionInterface;
+use webignition\JsonMessageSerializerBundle\Message\JsonSerializableMessageInterface;
 
 class DeleteMachine extends AbstractRemoteMachineRequest
 {
     use RetryableRequestTrait;
 
+    public const TYPE = 'delete-machine';
+
     public function getAction(): string
     {
         return RemoteRequestActionInterface::ACTION_DELETE;
+    }
+
+    public function getType(): string
+    {
+        return self::TYPE;
+    }
+
+    public function getPayload(): array
+    {
+        return array_merge(
+            parent::getPayload(),
+            [
+                'retry_count' => $this->getRetryCount(),
+            ]
+        );
+    }
+
+    public static function createFromArray(array $data): JsonSerializableMessageInterface
+    {
+        $machine = new DeleteMachine($data['machine_id']);
+        $machine->retryCount = $data['retry_count'];
+
+        return $machine;
     }
 }

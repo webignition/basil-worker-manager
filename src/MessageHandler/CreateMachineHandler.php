7e5f7f6518acf54c47dca3d1df068dee4e60b6ff
Jon Cram <webignition@gmail.com>
2021-03-31 14:51:53 +0100

    Use CreateFailureReasonFactory in CreateMachineHandler

--- src/MessageHandler/CreateMachineHandler.php
+++ src/MessageHandler/CreateMachineHandler.php
@@ -5,16 +5,44 @@ declare(strict_types=1);
 namespace App\MessageHandler;
 
 use App\Entity\Machine;
+use App\Exception\MachineProvider\ExceptionInterface;
+use App\Exception\UnsupportedProviderException;
 use App\Message\CheckMachineIsActive;
 use App\Message\CreateMachine;
+use App\MessageDispatcher\MachineRequestMessageDispatcher;
 use App\Model\Machine\State;
 use App\Model\RemoteMachineRequestSuccess;
 use App\Model\RemoteRequestOutcomeInterface;
 use App\Model\RemoteRequestSuccessInterface;
+use App\Repository\MachineRepository;
+use App\Services\CreateFailureFactory;
+use App\Services\ExceptionLogger;
+use App\Services\MachineProvider\MachineProvider;
+use App\Services\MachineStore;
+use App\Services\RemoteRequestRetryDecider;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class CreateMachineHandler extends AbstractRemoteMachineRequestHandler implements MessageHandlerInterface
 {
+    public function __construct(
+        MachineRepository $machineRepository,
+        MachineProvider $machineProvider,
+        RemoteRequestRetryDecider $retryDecider,
+        ExceptionLogger $exceptionLogger,
+        MachineStore $machineStore,
+        MachineRequestMessageDispatcher $dispatcher,
+        private CreateFailureFactory $createFailureFactory,
+    ) {
+        parent::__construct(
+            $machineRepository,
+            $machineProvider,
+            $retryDecider,
+            $exceptionLogger,
+            $machineStore,
+            $dispatcher
+        );
+    }
+
     public function __invoke(CreateMachine $message): RemoteRequestOutcomeInterface
     {
         return $this->handle(
@@ -36,10 +64,14 @@ class CreateMachineHandler extends AbstractRemoteMachineRequestHandler implement
 
                     $this->dispatcher->dispatch(new CheckMachineIsActive((string) $machine));
                 }
-            })->withFailureHandler(function (Machine $machine) {
-                $machine = $machine->setState(State::VALUE_CREATE_FAILED);
-                $this->machineStore->store($machine);
-            })
+            })->withFailureHandler(
+                function (Machine $machine, ExceptionInterface | UnsupportedProviderException $exception) {
+                    $machine = $machine->setState(State::VALUE_CREATE_FAILED);
+                    $this->machineStore->store($machine);
+
+                    $this->createFailureFactory->create($machine->getId(), $exception);
+                }
+            )
         );
     }
 }

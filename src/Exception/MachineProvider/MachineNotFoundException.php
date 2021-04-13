cf126080211a1d504f8220af506d1258f4575dd8
Jon Cram <webignition@gmail.com>
2021-04-13 17:05:21 +0100

    Add MachineNotFoundExceptionInterface::getId() (#461)

--- src/Exception/MachineProvider/MachineNotFoundException.php
+++ src/Exception/MachineProvider/MachineNotFoundException.php
@@ -2,18 +2,27 @@
 
 namespace App\Exception\MachineProvider;
 
-use webignition\BasilWorkerManagerInterfaces\MachineProviderInterface;
+use webignition\BasilWorkerManagerInterfaces\ProviderInterface;
 
 class MachineNotFoundException extends \Exception implements MachineNotFoundExceptionInterface
 {
+    /**
+     * @param ProviderInterface::NAME_* $providerName|null
+     */
     public function __construct(
-        private MachineProviderInterface $machineProvider
+        private string $id,
+        private ?string $providerName,
     ) {
         parent::__construct();
     }
 
-    public function getMachineProvider(): MachineProviderInterface
+    public function getId(): string
     {
-        return $this->machineProvider;
+        return $this->id;
+    }
+
+    public function getProviderName(): ?string
+    {
+        return $this->providerName;
     }
 }

bde52e02ba57d72c8f22f5c5a6f6260fa218bbcb
Jon Cram <webignition@gmail.com>
2021-04-13 15:02:45 +0100

    Factor out MachineProvider/Machine updating (#454)

---
+++ src/Services/MachineUpdater.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use webignition\BasilWorkerManager\PersistenceBundle\Services\Store\MachineStore;
+use webignition\BasilWorkerManagerInterfaces\MachineInterface;
+use webignition\BasilWorkerManagerInterfaces\RemoteMachineInterface;
+
+class MachineUpdater
+{
+    public function __construct(
+        private MachineStore $machineStore,
+    ) {
+    }
+
+    public function updateFromRemoteMachine(
+        MachineInterface $machine,
+        RemoteMachineInterface $remoteMachine
+    ): MachineInterface {
+        $remoteMachineState = $remoteMachine->getState();
+        $remoteMachineState = $remoteMachineState ?? MachineInterface::STATE_CREATE_REQUESTED;
+
+        $machine->setState($remoteMachineState);
+        $machine->setIpAddresses($remoteMachine->getIpAddresses());
+        $this->machineStore->store($machine);
+
+        return $machine;
+    }
+}

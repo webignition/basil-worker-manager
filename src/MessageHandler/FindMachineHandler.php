e00e603f1a1d3797f66fc37a5f59f9721045cc77
Jon Cram <webignition@gmail.com>
2021-04-14 16:37:44 +0100

    Create FindMachine message, FindMachineHandler (#479)

---
+++ src/MessageHandler/FindMachineHandler.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\MessageHandler;
+
+use App\Exception\MachineNotFoundException;
+use App\Message\FindMachine;
+use App\Services\ExceptionLogger;
+use App\Services\MachineUpdater;
+use App\Services\RemoteMachineFinder;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use webignition\BasilWorkerManager\PersistenceBundle\Entity\MachineProvider;
+use webignition\BasilWorkerManager\PersistenceBundle\Services\Store\MachineProviderStore;
+use webignition\BasilWorkerManager\PersistenceBundle\Services\Store\MachineStore;
+use webignition\BasilWorkerManagerInterfaces\MachineInterface;
+use webignition\SymfonyMessengerMessageDispatcher\MessageDispatcher;
+
+class FindMachineHandler implements MessageHandlerInterface
+{
+    public function __construct(
+        private MachineStore $machineStore,
+        private MachineProviderStore $machineProviderStore,
+        private RemoteMachineFinder $remoteMachineFinder,
+        private MachineUpdater $machineUpdater,
+        private MessageDispatcher $dispatcher,
+        private ExceptionLogger $exceptionLogger,
+    ) {
+    }
+
+    public function __invoke(FindMachine $message): void
+    {
+        $machineId = $message->getMachineId();
+
+        $machine = $this->machineStore->find($machineId);
+        if (!$machine instanceof MachineInterface) {
+            return;
+        }
+
+        $machine->setState(MachineInterface::STATE_FIND_FINDING);
+        $this->machineStore->store($machine);
+
+        try {
+            $remoteMachine = $this->remoteMachineFinder->find($machineId);
+
+            $this->machineUpdater->updateFromRemoteMachine($machine, $remoteMachine);
+
+            $machineProvider = new MachineProvider($machineId, $remoteMachine->getProvider());
+            $this->machineProviderStore->store($machineProvider);
+        } catch (MachineNotFoundException $machineNotFoundException) {
+            $envelope = $this->dispatcher->dispatch($message->incrementRetryCount());
+
+            if (false === MessageDispatcher::isDispatchable($envelope)) {
+                foreach ($machineNotFoundException->getExceptionStack() as $exception) {
+                    $this->exceptionLogger->log($exception);
+                }
+
+                $machine->setState(MachineInterface::STATE_FIND_NOT_FOUND);
+                $this->machineStore->store($machine);
+            }
+        }
+    }
+}

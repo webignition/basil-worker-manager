1e6d5729b5ad5d0cce482c39fa0c79f175c1b520
Jon Cram <webignition@gmail.com>
2021-04-15 16:34:34 +0100

    Create RemoteMachineRemover (#506)

---
+++ src/Exception/MachineNotRemovableException.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Exception;
+
+class MachineNotRemovableException extends AbstractMachineException
+{
+    /**
+     * @var \Throwable[]
+     */
+    private array $exceptionStack;
+
+    /**
+     * @param \Throwable[] $exceptionStack
+     */
+    public function __construct(
+        string $id,
+        array $exceptionStack = [],
+    ) {
+        parent::__construct($id);
+
+        $this->exceptionStack = array_filter($exceptionStack, function ($item) {
+            return $item instanceof \Throwable;
+        });
+    }
+
+    /**
+     * @return \Throwable[]
+     */
+    public function getExceptionStack(): array
+    {
+        return $this->exceptionStack;
+    }
+}

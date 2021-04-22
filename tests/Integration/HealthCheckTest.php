c24581350db143309947b36aa078c2fc342a8bbb
Jon Cram <webignition@gmail.com>
2021-04-22 16:52:40 +0100

    Add empty health check controller (#599)

---
+++ tests/Integration/HealthCheckTest.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration;
+
+class HealthCheckTest extends AbstractIntegrationTest
+{
+    public function testHealthCheck(): void
+    {
+        $response = $this->httpClient->get('/health-check');
+        self::assertSame(200, $response->getStatusCode());
+    }
+}

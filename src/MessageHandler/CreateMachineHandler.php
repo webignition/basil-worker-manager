f6715a7d12acdaf68faffba3558e91ba81730ed7
Jon Cram <webignition@gmail.com>
2021-04-22 15:40:17 +0100

    Re-implement CreateMachineHandler

--- src/MessageHandler/CreateMachineHandler.php
+++ src/MessageHandler/CreateMachineHandler.php
@@ -9,9 +9,6 @@ use App\Entity\MachineProvider;
 use App\Exception\MachineProvider\ExceptionInterface;
 use App\Exception\UnsupportedProviderException;
 use App\Message\CreateMachine;
-use App\Model\RemoteMachineRequestSuccess;
-use App\Model\RemoteRequestOutcomeInterface;
-use App\Model\RemoteRequestSuccessInterface;
 use App\Services\Entity\Factory\CreateFailureFactory;
 use App\Services\Entity\Store\MachineProviderStore;
 use App\Services\Entity\Store\MachineStore;
@@ -21,59 +18,70 @@ use App\Services\MachineRequestDispatcher;
 use App\Services\MachineUpdater;
 use App\Services\RemoteRequestRetryDecider;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use webignition\SymfonyMessengerMessageDispatcher\MessageDispatcher;
 
-class CreateMachineHandler extends AbstractRemoteMachineRequestHandler implements MessageHandlerInterface
+class CreateMachineHandler implements MessageHandlerInterface
 {
     public function __construct(
-        MachineManager $machineManager,
-        RemoteRequestRetryDecider $retryDecider,
-        ExceptionLogger $exceptionLogger,
-        MachineStore $machineStore,
-        MachineProviderStore $machineProviderStore,
-        MachineRequestDispatcher $machineRequestDispatcher,
+        private MachineManager $machineManager,
+        private RemoteRequestRetryDecider $retryDecider,
+        private ExceptionLogger $exceptionLogger,
+        private MachineStore $machineStore,
+        private MachineProviderStore $machineProviderStore,
+        private MachineRequestDispatcher $machineRequestDispatcher,
         private CreateFailureFactory $createFailureFactory,
         private MachineUpdater $machineUpdater,
     ) {
-        parent::__construct(
-            $machineManager,
-            $retryDecider,
-            $exceptionLogger,
-            $machineStore,
-            $machineProviderStore,
-            $machineRequestDispatcher,
-        );
     }
 
-    public function __invoke(CreateMachine $message): RemoteRequestOutcomeInterface
+    public function __invoke(CreateMachine $message): void
     {
-        return $this->handle(
-            $message,
-            (new RemoteMachineActionHandler(
-                function (MachineProvider $machineProvider) {
-                    return new RemoteMachineRequestSuccess(
-                        $this->machineManager->create($machineProvider)
-                    );
-                }
-            ))->withBeforeRequestHandler(function (Machine $machine) {
-                $machine->setState(Machine::STATE_CREATE_REQUESTED);
-                $this->machineStore->store($machine);
-            })->withSuccessHandler(
-                function (
-                    Machine $machine,
-                    RemoteRequestSuccessInterface $outcome
-                ) {
-                    if ($outcome instanceof RemoteMachineRequestSuccess) {
-                        $this->machineUpdater->updateFromRemoteMachine($machine, $outcome->getRemoteMachine());
-                    }
-                }
-            )->withFailureHandler(
-                function (Machine $machine, ExceptionInterface | UnsupportedProviderException $exception) {
-                    $machine->setState(Machine::STATE_CREATE_FAILED);
-                    $this->machineStore->store($machine);
+        $machine = $this->machineStore->find($message->getMachineId());
+        if (!$machine instanceof Machine) {
+            return;
+        }
 
-                    $this->createFailureFactory->create($machine->getId(), $exception);
+        $machineProvider = $this->machineProviderStore->find($message->getMachineId());
+        if (!$machineProvider instanceof MachineProvider) {
+            return;
+        }
+
+        $machine->setState(Machine::STATE_CREATE_REQUESTED);
+        $this->machineStore->store($machine);
+
+        $lastException = null;
+
+        try {
+            $remoteMachine = $this->machineManager->create($machineProvider);
+            $this->machineUpdater->updateFromRemoteMachine($machine, $remoteMachine);
+            $this->machineRequestDispatcher->dispatchCollection($message->getOnSuccessCollection());
+        } catch (ExceptionInterface $exception) {
+            $shouldRetry = $this->retryDecider->decide(
+                $machineProvider->getName(),
+                $message,
+                $exception->getRemoteException()
+            );
+
+            $lastException = $exception;
+
+            if ($shouldRetry) {
+                $envelope = $this->machineRequestDispatcher->reDispatch($message);
+
+                if (MessageDispatcher::isDispatchable($envelope)) {
+                    $lastException = null;
                 }
-            )
-        );
+            }
+        } catch (UnsupportedProviderException $unsupportedProviderException) {
+            $lastException = $unsupportedProviderException;
+        }
+
+        if ($lastException instanceof \Throwable) {
+            $machine->setState(Machine::STATE_CREATE_FAILED);
+            $this->machineStore->store($machine);
+
+            $this->createFailureFactory->create($machine->getId(), $lastException);
+
+            $this->exceptionLogger->log($lastException);
+        }
     }
 }

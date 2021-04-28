ab3b911977d5822d42fae32ce95da3d129309fc1
Jon Cram <webignition@gmail.com>
2021-04-28 14:56:50 +0100

    Fix CS

--- tests/Integration/HealthCheckTest.php
+++ tests/Integration/HealthCheckTest.php
@@ -4,19 +4,11 @@ declare(strict_types=1);
 
 namespace App\Tests\Integration;
 
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Exception\ServerException;
-
 class HealthCheckTest extends AbstractIntegrationTest
 {
     public function testHealthCheck(): void
     {
-        try {
-            $response = $this->httpClient->get('/health-check');
-        } catch (ServerException $e) {
-            echo "\n\n" . $e->getResponse()->getBody()->getContents() . "\n\n";
-        }
-        self::assertSame(200, $response->getStatusCode());
+        $response = $this->httpClient->get('/health-check');
 
         self::assertSame(
             [

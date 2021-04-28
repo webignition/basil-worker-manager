8c1671f2ebc160687b5295c9738cae873a3e1c5b
Jon Cram <webignition@gmail.com>
2021-04-28 14:30:17 +0100

    Create mockable component inspectors in ServiceStatusInspector tests

--- tests/Functional/Services/ServiceStatusInspector/ServiceStatusInspectorTest.php
+++ tests/Functional/Services/ServiceStatusInspector/ServiceStatusInspectorTest.php
@@ -4,21 +4,93 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Services\ServiceStatusInspector;
 
+use App\Services\ServiceStatusInspector\ComponentInspectorInterface;
 use App\Services\ServiceStatusInspector\ServiceStatusInspector;
 use App\Tests\AbstractBaseFunctionalTest;
+use Mockery\MockInterface;
+use webignition\ObjectReflector\ObjectReflector;
 
 class ServiceStatusInspectorTest extends AbstractBaseFunctionalTest
 {
-    public function testGet(): void
-    {
-        $serviceStatusInspector = self::$container->get(ServiceStatusInspector::class);
-        self::assertInstanceOf(ServiceStatusInspector::class, $serviceStatusInspector);
+    private ServiceStatusInspector $serviceStatusInspector;
 
-        self::assertEquals(
-            [
-                'database' => true,
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $serviceStatusInspector = self::$container->get(ServiceStatusInspector::class);
+        \assert($serviceStatusInspector instanceof ServiceStatusInspector);
+        $this->serviceStatusInspector = $serviceStatusInspector;
+    }
+
+    /**
+     * @dataProvider getDataProvider
+     *
+     * @param ComponentInspectorInterface[] $modifiedComponentInspectors
+     * @param array<string, bool> $expectedServiceStatus
+     */
+    public function testGet(array $modifiedComponentInspectors, array $expectedServiceStatus): void
+    {
+        foreach ($modifiedComponentInspectors as $name => $componentInspector) {
+            $this->setComponentInspector($name, $componentInspector);
+        }
+
+        self::assertEquals($expectedServiceStatus, $this->serviceStatusInspector->get());
+    }
+
+    /**
+     * @return array[]
+     */
+    public function getDataProvider(): array
+    {
+        return [
+            'all services available' => [
+                'modifiedComponentInspectors' => [],
+                'expectedServiceStatus' => [
+                    'database' => true,
+                ],
             ],
-            $serviceStatusInspector->get()
+            'database unavailable' => [
+                'modifiedComponentInspectors' => [
+                    'database' => $this->createComponentInspectorThrowingException(),
+                ],
+                'expectedServiceStatus' => [
+                    'database' => false,
+                ],
+            ],
+        ];
+    }
+
+    private function setComponentInspector(string $name, ComponentInspectorInterface $componentInspector): void
+    {
+        $componentInspectors = ObjectReflector::getProperty($this->serviceStatusInspector, 'componentInspectors');
+
+        if (array_key_exists($name, $componentInspectors)) {
+            $componentInspectors[$name] = $componentInspector;
+        }
+
+        ObjectReflector::setProperty(
+            $this->serviceStatusInspector,
+            ServiceStatusInspector::class,
+            'componentInspectors',
+            $componentInspectors
         );
+    }
+
+    private function createComponentInspectorThrowingException(): ComponentInspectorInterface
+    {
+        $componentInspector = $this->createComponentInspector();
+        if ($componentInspector instanceof MockInterface) {
+            $componentInspector
+                ->shouldReceive('__invoke')
+                ->andThrow(new \Exception());
+        }
+
+        return $componentInspector;
+    }
+
+    private function createComponentInspector(): ComponentInspectorInterface
+    {
+        return \Mockery::mock(ComponentInspectorInterface::class);
     }
 }
